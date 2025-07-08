0445d5e1622fa18123a83d1e2ab36cf01823c5e7
Maxwell Okosun <okosunmaxwell9@gmail.com>
2025-07-08 08:17:07 -0500

    Move BrowserRouter to index.tsx for Vercel SPA routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import GalleryPage from './pages/GalleryPage';
 import ImageDetailPage from './pages/ImageDetailPage';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<GalleryPage />} />
-        <Route path="/image/:id" element={<ImageDetailPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<GalleryPage />} />
+      <Route path="/image/:id" element={<ImageDetailPage />} />
+    </Routes>
   );
 }
 
